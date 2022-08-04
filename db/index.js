20b46479359bfbdc71906dde4e7a8c5060cf8a54
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-08-04 15:31:09 -0400

    update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let MONGODB_URI = "mongodb+srv://Shaded23:tJHue6g3PM1cqGtG@cluster0.dgov0dq.mongodb.net/plantsDatabase?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/plantsDatabase';
+let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/plantsDatabase';
 
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('Successfully connected to MongoDB'))
